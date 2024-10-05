b9bfc9dad7a80a55d40f0887f3cc27770b992b3f
sebastian <juansrangel89@gmail.com>
2024-10-04 20:24:20 -0500

    final commit y desliegue

--- cart.js
+++ cart.js
@@ -80,7 +80,7 @@ cart.forEach(product => {
     inputCant.id=product.id
     inputCant.value=product.cant
     inputCant.addEventListener('input', ()=>{
-        if (inputCant.value == 0 || inputCant.value === '' ) {
+        if (inputCant.value == 0 || inputCant.value === '' || isNaN(inputCant.value)  ) {
             inputCant.value = '1';
         }
         inputCant.value = inputCant.value.replace(/[^0-9]/g, '');
